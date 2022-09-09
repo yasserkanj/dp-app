33d7ec94abe57f3530d1d3f8a6e22be1b0bf7a3b
YasserAmmarKanj <yasser.ammar.kanj@gmail.com>
2022-09-09 14:20:21 +0300

    init

---
+++ lang/en/response.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'added_successfully'    => 'Record has been added successfully',
+    'updated_successfully'  => 'Record has been updated successfully',
+    'deleted_successfully'  => 'Record has been deleted successfully',
+    'trashed_successfully'  => 'Record has been trashed successfully',
+    'restored_successfully' => 'Record has been restored successfully',
+    'not_allowed'           => 'Method not allowed',
+    'not_authorized'        => 'Action not authorized',
+    'not_authenticated'     => 'You are not',
+    'not_found'             => 'Resource not found',
+    'user_not_found'        => 'User not found',
+    'wrong_password'        => 'Invalid password',
+    'success'               => 'Success',
+    'failed'                => 'Failed',
+    'login_successfully'    => 'Logged in successfully',
+    'logout_successfully'   => 'Logout successfully',
+    'login_failed'          => 'Login failed!',
+    'must_login'            => 'You must sign in before',
+    
+    'customer_does_not_have_users' => 'Customer does not have users'
+];
+
+
+
+
+
+
+
+
+
+
